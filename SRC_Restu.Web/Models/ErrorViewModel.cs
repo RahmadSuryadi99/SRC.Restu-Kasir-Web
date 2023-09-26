82b762f554ed256efe5c169772248b9d44237176
YadiLaptopIglo <Rahmadtkd13@gmail.com>
2023-09-26 09:33:46 +0700

    Add project files.

---
+++ SRC_Restu.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SRC_Restu.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
